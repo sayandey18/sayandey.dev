1526ee63a00884502b306142bd864fbf027c7728
Sayan Dey <roboticsayan@outlook.com>
2022-09-26 00:14:23 +0530

    feat: Update footer links

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -28,6 +28,16 @@ export default function Footer() {
               About
             </a>
           </Link>
+          <Link href="/projects">
+            <a className="text-gray-500 hover:text-gray-600 transition">
+              Projects
+            </a>
+          </Link>
+          <Link href="/utilities">
+            <a className="text-gray-500 hover:text-gray-600 transition">
+              Utilities
+            </a>
+          </Link>
           <Link href="/newsletter">
             <a className="text-gray-500 hover:text-gray-600 transition">
               Newsletter
@@ -35,10 +45,18 @@ export default function Footer() {
           </Link>
         </div>
         <div className="flex flex-col space-y-4">
+          <ExternalLink href="https://github.com/sayandey18">
+            GitHub
+          </ExternalLink>
+          <ExternalLink href="https://www.linkedin.com/in/iam-sayandey">
+            LinkedIn
+          </ExternalLink>
           <ExternalLink href="https://twitter.com/wpblogmetrics">
             Twitter
           </ExternalLink>
-          <ExternalLink href="https://github.com/sayandey18">GitHub</ExternalLink>
+          <ExternalLink href="https://www.instagram.com/iam.sayandey/">
+            Instagram
+          </ExternalLink>
           <ExternalLink href="https://www.youtube.com/channel/UCZMli3czZnd1uoc1ShTouQw">
             YouTube
           </ExternalLink>
