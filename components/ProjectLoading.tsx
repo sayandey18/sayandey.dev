18f10f2568ab0a40d94e402aa1c81586108e15ee
Sayan Dey <roboticsayan@outlook.com>
2022-09-26 00:13:30 +0530

    feat: Integrate primary color and minor UI fixes

---
+++ components/ProjectLoading.tsx
@@ -0,0 +1,73 @@
+export default function ProjectLoading() {
+  return (
+    <div className="flex flex-wrap w-full gap-y-5">
+      <div
+        role="status"
+        className="flex flex-col justify-center animate-pulse dark:bg-black-charcoal shadow-[0_2px_16px_rgb(95_95_95_/_10%)] space-y-2.5 rounded-lg p-6 w-full h-32"
+      >
+        <div className="flex items-center space-x-2 w-11/12">
+          <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-600 w-4/12"></div>
+          <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-600 w-full"></div>
+        </div>
+
+        <div className="flex items-center space-x-2 w-full">
+          <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-600 w-full"></div>
+        </div>
+
+        <div className="flex items-center space-x-2 w-full">
+          <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-600 w-10/12"></div>
+        </div>
+
+        <div className="flex items-center space-x-2 w-9/12">
+          <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 w-2/4"></div>
+          <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-600 w-2/4"></div>
+        </div>
+      </div>
+      <div
+        role="status"
+        className="flex flex-col justify-center animate-pulse dark:bg-black-charcoal shadow-[0_2px_16px_rgb(95_95_95_/_10%)] space-y-2.5 rounded-lg p-6 w-full h-32"
+      >
+        <div className="flex items-center space-x-2 w-11/12">
+          <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-600 w-4/12"></div>
+          <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-600 w-full"></div>
+        </div>
+
+        <div className="flex items-center space-x-2 w-full">
+          <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-600 w-full"></div>
+        </div>
+
+        <div className="flex items-center space-x-2 w-full">
+          <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-600 w-10/12"></div>
+        </div>
+
+        <div className="flex items-center space-x-2 w-9/12">
+          <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 w-2/4"></div>
+          <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-600 w-2/4"></div>
+        </div>
+      </div>
+      <div
+        role="status"
+        className="flex flex-col justify-center animate-pulse dark:bg-black-charcoal shadow-[0_2px_16px_rgb(95_95_95_/_10%)] space-y-2.5 rounded-lg p-6 w-full h-32"
+      >
+        <div className="flex items-center space-x-2 w-11/12">
+          <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-600 w-4/12"></div>
+          <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-600 w-full"></div>
+        </div>
+
+        <div className="flex items-center space-x-2 w-full">
+          <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-600 w-full"></div>
+        </div>
+
+        <div className="flex items-center space-x-2 w-full">
+          <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-600 w-10/12"></div>
+        </div>
+
+        <div className="flex items-center space-x-2 w-9/12">
+          <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 w-2/4"></div>
+          <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-600 w-2/4"></div>
+        </div>
+      </div>
+      <span className="sr-only">Projects Loading...</span>
+    </div>
+  );
+}
