bfc7cb790dadd7f7c858c7ed5c9933128af73863
Sayan Dey <roboticsayan@outlook.com>
2022-09-28 00:09:48 +0530

    fix: Resolve spotify now playing

--- lib/spotify.ts
+++ lib/spotify.ts
@@ -2,6 +2,7 @@ const client_id = process.env.SPOTIFY_CLIENT_ID;
 const client_secret = process.env.SPOTIFY_CLIENT_SECRET;
 const refresh_token = process.env.SPOTIFY_REFRESH_TOKEN;
 
+const basic = btoa(`${client_id}:${client_secret}`);
 const NOW_PLAYING_ENDPOINT = `https://api.spotify.com/v1/me/player/currently-playing`;
 const TOP_TRACKS_ENDPOINT = `https://api.spotify.com/v1/me/top/tracks`;
 const TOKEN_ENDPOINT = `https://accounts.spotify.com/api/token`;
@@ -10,9 +11,7 @@ const getAccessToken = async () => {
   const response = await fetch(TOKEN_ENDPOINT, {
     method: 'POST',
     headers: {
-      Authorization: `Basic ${Buffer.from(
-        `${client_id}:${client_secret}`
-      ).toString('base64')}`,
+      Authorization: `Basic ${basic}`,
       'Content-Type': 'application/x-www-form-urlencoded'
     },
     body: new URLSearchParams({
