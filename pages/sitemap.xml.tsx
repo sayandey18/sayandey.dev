08cd5e47a8ed50918b92cd5f1caad1aefcd22341
Sayan Dey <roboticsayan@outlook.com>
2022-10-24 21:41:53 +0530

    Update new pages in `sitemap.xml`

--- pages/sitemap.xml.tsx
+++ pages/sitemap.xml.tsx
@@ -25,8 +25,10 @@ export async function getServerSideProps({ res }) {
       'dashboard',
       'projects',
       'utilities',
+      'resources',
       'guestbook',
       'newsletter',
+      'certificates',
       'tweets',
       'uses'
     ]
