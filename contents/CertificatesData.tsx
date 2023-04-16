0073a6f5d05a9c4b807d7d02b26849c572960ec3
Sayan Dey <roboticsayan@outlook.com>
2023-04-16 23:51:20 +0530

    Update new certificate

--- contents/CertificatesData.tsx
+++ contents/CertificatesData.tsx
@@ -5,12 +5,20 @@ const certdata = {
   certlist: {
     data: [
       {
-        title: 'Geoprocessing using Python',
+        title: 'Geoprocessing Using Python',
         issuedDate: 'Feb 8, 2021',
         issuedBy: 'ISRO',
         orgLogo: 'https://i.imgur.com/WS0ZDER.png',
         pdfUrl:
           'https://drive.google.com/file/d/1T1V7M6dtAEBEq-SSWoCwgnHXYIOZEB9w/view?usp=sharing'
+      },
+      {
+        title: 'Frontend Development',
+        issuedDate: 'April 16, 2023',
+        issuedBy: 'Great Learning',
+        orgLogo: 'https://i.imgur.com/zU7Tp4E.png',
+        pdfUrl:
+          'https://drive.google.com/file/d/1r2ne4jg3VzNGFTMg1G2xhzPH_3bHYjUa/view?usp=sharing'
       }
     ]
   }
