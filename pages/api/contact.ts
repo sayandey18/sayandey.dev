d2e76b0f91e9ab4d8d26cd5100794e290f318d5c
Sayan Dey <roboticsayan@outlook.com>
2023-04-16 23:52:35 +0530

    Change `from` email to `@admin`

--- pages/api/contact.ts
+++ pages/api/contact.ts
@@ -24,7 +24,7 @@ export default async function handler(
   }
 
   const mailData = {
-    from: 'Sayan Dey <no-reply@sayandey.dev>',
+    from: 'Sayan Dey <admin@sayandey.dev>',
     to: 'Sayan Dey <sayan@sayandey.dev>',
     replyTo: email,
     subject: `Message from ${fname}`,
