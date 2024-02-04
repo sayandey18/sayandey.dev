088ab628af1c0c075194f3f33b17a7efd454e9ee
Sayan <mr.sayandey18@outlook.com>
2024-02-04 23:57:28 +0530

    Update SANITY api level

--- lib/sanity-config.ts
+++ lib/sanity-config.ts
@@ -2,5 +2,5 @@ export const sanityConfig = {
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || 'production',
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || 'q0pwph59',
   useCdn: process.env.NODE_ENV === 'production',
-  apiVersion: '2023-09-04',
+  apiVersion: '2024-02-04',
 };
